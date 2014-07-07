e8a57f874b8ced9a54a991802257f1eb70aeef85
Alex Mikhalev <alexmikhalevalex@gmail.com>
2014-07-07 15:54:00 -0600

    Converted the Livereload script to use proper angularjs

--- app/scripts/dev.js
+++ app/scripts/dev.js
@@ -5,13 +5,16 @@
 'use strict';
 
 // Livereload
-var net = require('net');
-var client = net.connect(9292);
-client.on('data', function (data) {
-  if (data.toString() === 'reload') {
-    window.location.reload(true);
-  }
-});
-client.on('error', function () {
-  // Do nothing, probably not launched from gulp
+angular.module('TatorDashboard')
+  .run(function ($window) {
+  var net = require('net');
+  var client = net.connect(9292);
+  client.on('data', function (data) {
+    if (data.toString() === 'reload') {
+      $window.location.reload(true);
+    }
+  });
+  client.on('error', function () {
+    // Do nothing, probably not launched from gulp
+  });
 });
