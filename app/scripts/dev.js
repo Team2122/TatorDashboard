38bf249daeca63d066b75cbb3d080768f360ab60
Alex Mikhalev <alexmikhalevalex@gmail.com>
2014-07-07 17:38:34 -0600

    Fixed Livereload

--- app/scripts/dev.js
+++ app/scripts/dev.js
@@ -6,15 +6,15 @@
 
 // Livereload
 angular.module('TatorDashboard')
-  .run(function ($window) {
-  var net = require('net');
-  var client = net.connect(9292);
-  client.on('data', function (data) {
-    if (data.toString() === 'reload') {
-      $window.location.reload(true);
-    }
+  .run(function ($route) {
+    var net = require('net');
+    var client = net.connect(9292);
+    client.on('data', function (data) {
+      if (data.toString() === 'reload') {
+        $route.reload(true);
+      }
+    });
+    client.on('error', function () {
+      // Do nothing, probably not launched from gulp
+    });
   });
-  client.on('error', function () {
-    // Do nothing, probably not launched from gulp
-  });
-});
